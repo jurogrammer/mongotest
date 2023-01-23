2e64eee9ab4b84473f00863223d355afa4497e4d
injae-ju <injae.ju@navercorp.com>
2023-01-23 14:43:58 +0900

    init

---
+++ src/test/java/juro/mongotest/MongotestApplicationTests.java
@@ -0,0 +1,13 @@
+package juro.mongotest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongotestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
